ff1c48e9ce7adbc8c10d6ba1978be66332c3f5db
javoxhr2oo8 <javohirblogg3r@gmail.com>
2026-02-14 19:30:14 +0300

    added meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,8 @@ export default defineNuxtConfig({
   // ],
   app: {
     head: {
+      viewport: 'width=device-width, initial-scale=1',
+      charset: 'utf-8',
       link: [
         {
           rel: 'stylesheet',
