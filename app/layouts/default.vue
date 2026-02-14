bf9f7c21373f2d7da77372402e04adfb3095c326
javoxhr2oo8 <javohirblogg3r@gmail.com>
2026-02-14 19:10:25 +0300

    removed the user pre

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,16 @@
 <script setup>
+const { user, showAlert, closeApp, tg } = useTelegram();
 
+onMounted(() => {
+    if (tg) {
+        tg.ready();
+        tg.expand();
+    }
+});
 </script>
 
 <template>
     <div>
-        <NuxtPage/>
+        <NuxtPage />
     </div>
 </template>
