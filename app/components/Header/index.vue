6e22a90641d4c68a0ec1432858855d88df4e5766
javoxhr2oo8 <javohirblogg3r@gmail.com>
2026-02-15 08:19:36 +0300

    added upgrade

--- app/components/Header/index.vue
+++ app/components/Header/index.vue
@@ -2,8 +2,6 @@
 import Button from '../Ui/Button.vue';
 import Card from '../Ui/Card.vue';
 import FlexBox from '../Ui/FlexBox.vue';
-
-const {user} = useTelegram()
 </script>
 
 <template>
@@ -15,7 +13,7 @@ const {user} = useTelegram()
                     <div class="header-right">
                         <FlexBox items="center" gap="10px">
                             <Button class="reload-app-btn"><i class="fas fa-sync"></i></Button>
-                                <img :src="user ? user?.photo_url :  'https://cdn3.iconfinder.com/data/icons/basicolor-essentials/24/051_user_profile_avatar-1024.png' " alt="">
+                                <img src="'https://cdn3.iconfinder.com/data/icons/basicolor-essentials/24/051_user_profile_avatar-1024.png'" alt="">
                         </FlexBox> 
                     </div>
                 </FlexBox>
