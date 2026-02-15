53dbb460b75f2b52c0b01b56f55b0d63cc08650c
javoxhr2oo8 <javohirblogg3r@gmail.com>
2026-02-15 08:23:52 +0300

    added pre

--- app/components/Header/index.vue
+++ app/components/Header/index.vue
@@ -17,6 +17,7 @@ const {user} = useTelegram()
                             <Button class="reload-app-btn"><i class="fas fa-sync"></i></Button>
                             <img v-if="user" :src="user?.photo_url" alt="">
                             <img v-else src="https://cdn3.iconfinder.com/data/icons/basicolor-essentials/24/051_user_profile_avatar-1024.png" alt="">
+                            <pre>{{ user?.photo_url }}</pre>
                         </FlexBox> 
                     </div>
                 </FlexBox>
